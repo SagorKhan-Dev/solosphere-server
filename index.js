af64722928741a477e54164cb088604c0dca16cd
SagorKhan-Dev <sagorkhan.cit@gmail.com>
2024-05-06 13:13:12 +0600

    get single job data and post bid data in database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -33,7 +33,23 @@ async function run() {
     // Get all jobs data from db
     app.get("/jobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
-      res.send(result)
+      res.send(result);
+    });
+
+    // Get a single job data using job id
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    //! Bids data related word
+    // Save a bid data in database
+    app.post("/bid", async (req, res) => {
+      const bidData = req.body;
+      const result = await bidsCollection.insertOne(bidData);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
